e4eaca5f815860e97e022187c4ba3772e2fa196a
hebbarsjk <hebbarsjk@gmail.com>
2021-08-13 12:32:53 +0530

    basic loginpage ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
-import 'package:api_practice/screens/Home/home_page.dart';
-import 'package:api_practice/screens/Standings/standings_page.dart';
-import 'package:api_practice/screens/Drivers/drivers_page.dart';
+// import 'package:api_practice/screens/Home/home_page.dart';
+// import 'package:api_practice/screens/Standings/standings_page.dart';
+// import 'package:api_practice/screens/Drivers/drivers_page.dart';
+import 'package:api_practice/screens/Authentication/login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -26,54 +29,79 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  final Future<FirebaseApp> _intialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Formula One Stats'),
-      ),
-      body: DefaultTabController(
-        length: 3,
-        child: Stack(
-          children: [
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-            ),
-            Scaffold(
-              bottomNavigationBar: Padding(
-                padding: EdgeInsets.only(bottom: 15.0),
-                child: TabBar(
-                  tabs: [
-                    Tab(
-                      icon: Icon(Icons.circle),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.play_arrow),
-                    ),
-                    Tab(
-                      icon: Icon(Icons.person),
-                    ),
-                  ],
-                  labelColor: Colors.red,
-                  indicator: UnderlineTabIndicator(
-                    borderSide: BorderSide(color: Colors.red, width: 4.0),
-                    insets: EdgeInsets.only(bottom: 44),
-                  ),
-                  unselectedLabelColor: Colors.grey,
-                ),
-              ),
-              body: TabBarView(
-                children: [
-                  HomePage(),
-                  StandingsPage(),
-                  DriversPage(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+    return FutureBuilder(
+      future: _intialization,
+      builder: (context, snapshot) {
+        // checking for errors
+        if (snapshot.hasError) {
+          print('Error');
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LogIn();
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+// Scaffold(
+//       appBar: AppBar(
+//         title: Text('Formula One Stats'),
+//       ),
+//       body: DefaultTabController(
+//         length: 3,
+//         child: Stack(
+//           children: [
+//             Container(
+//               height: double.infinity,
+//               width: double.infinity,
+//             ),
+//             Scaffold(
+//               bottomNavigationBar: Padding(
+//                 padding: EdgeInsets.only(bottom: 15.0),
+//                 child: TabBar(
+//                   tabs: [
+//                     Tab(
+//                       icon: Icon(Icons.circle),
+//                     ),
+//                     Tab(
+//                       icon: Icon(Icons.play_arrow),
+//                     ),
+//                     Tab(
+//                       icon: Icon(Icons.person),
+//                     ),
+//                   ],
+//                   labelColor: Colors.red,
+//                   indicator: UnderlineTabIndicator(
+//                     borderSide: BorderSide(color: Colors.red, width: 4.0),
+//                     insets: EdgeInsets.only(bottom: 44),
+//                   ),
+//                   unselectedLabelColor: Colors.grey,
+//                 ),
+//               ),
+//               body: TabBarView(
+//                 children: [
+//                   HomePage(),
+//                   StandingsPage(),
+//                   DriversPage(),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
---
+++ lib/screens/Authentication/login.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Login'),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email',
+                  ),
+                  validator: (String value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field cannot be empty';
+                    }
+                    return null;
+                  },
+                  controller: _emailController,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                  ),
+                  validator: (String value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Field cannot be empty';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  controller: _passwordController,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: ElevatedButton(
+                    child: Text('Submit'),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        var email = _emailController.text;
+                        var password = _passwordController.text;
+                        print('Email - $email and Password - $password');
+                      } else {
+                        print('Invalid');
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
