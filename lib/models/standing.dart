7a2bc74e559aeb0f674a1b479b0908e942e64aaf
hebbarsjk <hebbarsjk@gmail.com>
2021-08-22 19:45:50 +0530

    added model for driver standings

---
+++ lib/models/standing.dart
@@ -0,0 +1,24 @@
+class Standing {
+  String position,
+      points,
+      wins,
+      driverID,
+      permanentNumber,
+      code,
+      givenName,
+      lastName,
+      dateOfBirth,
+      nationality;
+
+  Standing(
+      this.position,
+      this.points,
+      this.wins,
+      this.driverID,
+      this.permanentNumber,
+      this.code,
+      this.givenName,
+      this.lastName,
+      this.dateOfBirth,
+      this.nationality);
+}
--- lib/screens/Standings/standings_page.dart
+++ lib/screens/Standings/standings_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:api_practice/models/standing.dart';
 
 class StandingsPage extends StatefulWidget {
   @override
@@ -6,6 +11,33 @@ class StandingsPage extends StatefulWidget {
 }
 
 class _StandingsPageState extends State<StandingsPage> {
+  Future getStandingData() async {
+    var response = await http
+        .get(Uri.http('ergast.com', 'api/f1/2020/driverStandings.json'));
+    var decoded = jsonDecode(response.body);
+    var jsonData =
+        decoded["MRData"]["StandingsTable"]["StandingsList"]["DriverStandings"];
+
+    List<Standing> standings = [];
+
+    for (var standing in jsonData) {
+      print("Circuit");
+      Standing c = Standing(
+          standing['position'],
+          standing['points'],
+          standing['wins'],
+          standing['Driver']['driverID'],
+          standing['Driver']['permanentNumber'],
+          standing['Driver']['code'],
+          standing['Driver']['givenName'],
+          standing['Driver']['lastName'],
+          standing['Driver']['dateOfBirth'],
+          standing['Driver']['nationality']);
+      standings.add(c);
+    }
+    return standings;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
